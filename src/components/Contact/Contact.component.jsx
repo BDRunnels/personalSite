90bdc00a02a0a2178e06e5559da943bbcb9eda5f
Bryan Runnels <bryan.d.runnels@gmail.com>
2024-05-08 10:25:37 -0500

    Removed info

--- src/components/Contact/Contact.component.jsx
+++ src/components/Contact/Contact.component.jsx
@@ -6,7 +6,6 @@ const Contact = () => {
                 <Card className="mx-auto dark:bg-black dark:border-2 dark:border-white max-w-[90vw] ">
                     <CardBody className="">
                         <a href="mailto:bryan.d.runnels@gmail.com" className=" font-bold dark:text-white text-black text-xl hover:underline">bryan.d.runnels@gmail.com</a>
-                        <h1 className="font-bold dark:text-white text-black text-xl text-center mt-2"> 816.853.7789 </h1>
                         <h2 className="text-center text-xl text-black dark:text-white mt-2"> Kansas, USA </h2>
                         <h2 className="text-center text-xl text-black dark:text-white mt-2"> Open to Relocation </h2>
                     </CardBody>
